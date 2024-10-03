ca42126ff5d69b31f1a71a505c844e4996170a76
Shreyas Pangavhane <147264785+shreyaspangavhane@users.noreply.github.com>
2024-10-03 15:23:25 +0530

    Update 01_First_03.py

--- 01_First_03.cpp
+++ 01_First_03.cpp
@@ -1,6 +1,32 @@
-/*Given two arrays S[] and E[] of size N denoting starting 
-and closing time of the shops and an integer value K denoting the 
-number of people, the task is to find out the maximum number of 
-shops they can visit in total if they visit each shop optimally based 
-on the following conditions:*/
-#include <iostream>
+#Implement a problem of smallest no with atleast n trailing zeroes in factorial.
+
+def trailing_zeroes(n):
+    #Initialize count
+    count = 0
+    #Start with 5 (since 5 is the factor responsible for trailing zeroes in factorials)
+    i = 5
+    #Loop to count how many times n can be divided by powers of 5
+    while n // i >= 1:
+        count += n//i
+        i*=5
+    return count
+
+def smallest_no(n):
+    #Initialize binary search range
+    low, high = 0, 5*n 
+    while low < high:
+        #Calculate the midpoint of the current range
+        mid=(low+high)//2
+        
+        #Check the number of trailing zeroes in mid!
+        if trailing_zeroes(mid)>=n:
+            #If mid has enough trailing zeroes,narrow search to the left half
+            high = mid
+        else:
+            #Otherwise, search in the right half
+            low = mid+1
+    #When the loop ends, low == high, which is the smallest number with at least n trailing zeroes
+    return low
+
+n = int(input("Enter the number of trailing zeroes: "))
+print("Smallest number whose factorial contains at least",n,"trailing zeroes is:", smallest_no(n))
